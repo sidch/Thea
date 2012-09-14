cd31ed931dc0b547a9eb2010344703590a81844d
sidch <sidch@pyg>
2012-09-14 03:50:55 -0400

    Update patches and fix install script
    Former-commit-id: 7b61faa8deef88df566b72fbe26bef6e9882c395

--- Code/Dependencies/G3D/create_patches.sh
+++ Code/Dependencies/G3D/create_patches.sh
@@ -2,7 +2,7 @@
 
 cd G3D
 
-REF_DIR="/Volumes/BOOTCAMP/Users/sid/Documents/GitHub/Fuse_OldWin/Code/Dependencies/Thea/Code/Dependencies/G3D/G3D/"
+REF_DIR="$HOME/Downloads/G3D"
 FILES=`find G3D.lib \( -name '*pp' -or -name '*.h' \) -exec diff -U3 {} $REF_DIR/{} \; | grep '^---' | cut -d ' ' -f 2 | cut -f 1`
 
 echo "$FILES"
