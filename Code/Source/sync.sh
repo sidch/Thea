60635aaeaab9340c8feee82a55737757737964f3
sidch <53786+sidch@users.noreply.github.com>
2012-10-14 04:36:43 -0400

    Make it a bit clearer what's going on when syncing
    Former-commit-id: 702f16466378112684201aba2ffca6705da4a5db

--- Code/Source/sync.sh
+++ Code/Source/sync.sh
@@ -10,6 +10,6 @@ TGT_DIR=$1
 FILES=`find . -name '*.[hc]pp' -exec diff -q {} $TGT_DIR/{} \; 2>&1 | cut -d ' ' -f 2 | grep -o '\./.*pp'`
 for f in $FILES
 do
-  echo "Syncing $f to $TGT_DIR/$f"
+  echo "Copying $f to $TGT_DIR/$f"
   cp $f "$TGT_DIR/$f"
 done
