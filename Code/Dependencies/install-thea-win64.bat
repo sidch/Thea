a20ae047f40d67884135a10f07e7ab528a31e3a3
sidch <53786+sidch@users.noreply.github.com>
2014-01-08 01:20:36 -0500

    No more bundled deps!
    Former-commit-id: b784317cfb4f9a1403918b53d9ffcc36351383e5

---
+++ Code/Dependencies/install-thea-win64.bat
@@ -0,0 +1,32 @@
+@echo off
+
+echo !!! Please ensure all support libraries have already been installed !!!
+echo.
+echo To specify a installation directory for Thea other than the directory
+echo containing this script, pass this directory as the first argument to
+echo the script. This should also be the directory containing the support
+echo libraries.
+echo.
+echo Installing Thea...
+
+pushd %~dp0
+
+if "%1"=="" (
+  set THEA_PREFIX="%CD%"
+) else (
+  set THEA_PREFIX="%1"
+)
+
+cd ..\Thea\Code\Build
+rem cmake "-DCMAKE_INSTALL_PREFIX=%THEA_PREFIX%" -DBOOST_ROOT=C:\Boost32 -DQT_QMAKE_EXECUTABLE=C:\Qt32\4.8.2\bin\qmake.exe -DCGAL_ROOT=C:\CGAL-3.7-32 -G "Visual Studio 10 Win64" .
+cmake "-DCMAKE_INSTALL_PREFIX=%THEA_PREFIX%" -G "Visual Studio 10 Win64" .
+
+devenv /Build Release Thea.sln
+devenv /Build Release /Project INSTALL Thea.sln
+
+devenv /Build Debug Thea.sln
+devenv /Build Debug /Project INSTALL Thea.sln
+
+set THEA_PREFIX=
+
+popd
