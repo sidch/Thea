99c16b6e2470925a373a6f4fc947a080c943f840
sidch <sidch@pyg>
2014-01-08 01:20:36 -0500

    No more bundled deps!
    Former-commit-id: b784317cfb4f9a1403918b53d9ffcc36351383e5

---
+++ Code/Dependencies/install-thea-win32-express.bat
@@ -0,0 +1,31 @@
+@echo off
+
+echo !!! Please ensure all support libraries have already been installed !!!
+echo.
+echo To specify a installation directory for Thea other than the directory
+echo containing this script, pass this directory as the first argument to
+echo the script. This should also be the directory containing the support
+echo libraries.
+echo.
+echo Installing Thea...
+
+pushd %~dp0
+
+if "%1"=="" (
+  set THEA_PREFIX="%CD%"
+) else (
+  set THEA_PREFIX="%1"
+)
+
+cd ..\Thea\Code\Build
+cmake "-DCMAKE_INSTALL_PREFIX=%THEA_PREFIX%" -G "Visual Studio 10" .
+
+msbuild Thea.sln /p:Configuration="Release"
+msbuild INSTALL.vcxproj /p:Configuration="Release"
+
+msbuild Thea.sln /p:Configuration="Debug"
+msbuild INSTALL.vcxproj /p:Configuration="Debug"
+
+set THEA_PREFIX=
+
+popd
